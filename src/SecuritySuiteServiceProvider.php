33711fcd0ac99a338f908fda2fea63cc7eea7b76
Dileep-sk <dileep@skylineinfosys.com>
2025-11-22 23:30:34 +0530

    Update Package

--- src/SecuritySuiteServiceProvider.php
+++ src/SecuritySuiteServiceProvider.php
@@ -1,22 +1,26 @@
 <?php
 
-namespace SecurityLabs\LaravelSecurityChecker;
+namespace SecurityLabs\LaravelSecuritySuite;
 
 use Illuminate\Support\ServiceProvider;
-use SecurityLabs\LaravelSecurityChecker\Commands\RunSecurityScan;
+use SecurityLabs\LaravelSecuritySuite\Commands\RunSecurityScan;
 
-class SecurityCheckerServiceProvider extends ServiceProvider
+class SecuritySuiteServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/security-checker.php', 'security-checker');
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/security-checker.php',
+            'security-checker'
+        );
     }
 
     public function boot()
     {
         if ($this->app->runningInConsole()) {
+
             $this->publishes([
-                __DIR__.'/../config/security-checker.php' => config_path('security-checker.php'),
+                __DIR__ . '/../config/security-checker.php' => config_path('security-checker.php'),
             ], 'security-checker-config');
 
             $this->commands([
